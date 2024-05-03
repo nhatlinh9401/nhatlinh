c1483a322483052fac08bd0b61586eba41002691
nhatlinh94 <linhtong01237242961@gmail.com>
2024-05-03 20:05:39 +0700

    tonglinh

---
+++ boostraps/bootstrap.js
@@ -0,0 +1,14 @@
+// alert ("hello world");
+console.log("hello world");
+// confirm("hello world");
+// prompt("mày là ai?");
+
+
+var name = "John";
+let hello = 3 ;
+alert (hello)
+
+let a = 5;
+let b = 6;
+let kq = a*b;
+console.log(kq);
